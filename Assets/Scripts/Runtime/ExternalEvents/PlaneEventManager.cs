da17501819f4f09b6643d6bfe8b15fc81d3cb508
Tsem <tom.segaux60@gmail.com>
2024-10-24 17:58:39 +0200

    [ADD] jps

--- Assets/Scripts/Runtime/ExternalEvents/PlaneEventManager.cs
+++ Assets/Scripts/Runtime/ExternalEvents/PlaneEventManager.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlaneEventManager : MonoBehaviour
 {
     [SerializeField] private PlaneEventBehaviour _planePrefab;
     [SerializeField] private PlaneEventPaths _paths;
-    private void Update()
+
+    [SerializeField] private float _minSpawnTime;
+    [SerializeField] private float _maxSpawnTime;
+
+    public UnityEvent OnPlaneSpawed; 
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            SpawnPlane();
-        }
+        StartCoroutine(SpawnPlaneEventRoutine());
     }
 
     public void SpawnPlane()
     {
+        OnPlaneSpawed?.Invoke();
         PlaneEventBehaviour plane = Instantiate(_planePrefab, transform);
         plane.Init(_paths);
+    }
+
+    private
+    
+    private IEnumerator SpawnPlaneEventRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(_minSpawnTime, _maxSpawnTime));
+            SpawnPlane();
+            yield return null;
+        }
     }
 }
