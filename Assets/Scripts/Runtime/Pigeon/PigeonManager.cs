4d4700f79230be78343933e6a831df307b7ab57b
LeopoldLecl <leopold.leclerc@edu.devinci.fr>
2024-10-24 11:17:35 +0200

    pigeon sounds

--- Assets/Scripts/Runtime/Pigeon/PigeonManager.cs
+++ Assets/Scripts/Runtime/Pigeon/PigeonManager.cs
@@ -18,6 +18,7 @@ public class PigeonManager : MonoBehaviour
     [SerializeField] private PigeonPaths _pigeonPaths;
     [SerializeField] private PigeonBehaviour[] _pigeonPrefabs;
     [SerializeField] private AudioClip _pigeonSound;
+    [SerializeField] private AudioClip _pigeonFlyAwaySound;
     [SerializeField] private List<PigeonSlot> _pigeonSlots = new List<PigeonSlot>();
     
     public int PigeonAmountOnPerch { get; private set; }
@@ -45,6 +46,8 @@ public class PigeonManager : MonoBehaviour
             pigeon.currentPigeon = null;
             JoyconRumblingManager.Instance.OnRumbleStop?.Invoke(pigeon.Localisation);
         }
+        AudioSource.PlayClipAtPoint(_pigeonFlyAwaySound, Camera.main.transform.position);
+
     }
 
     public bool TrySpawnPigeon()
